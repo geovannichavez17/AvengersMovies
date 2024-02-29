3732536c235105704ddecd3f4d9e523b8643e7fa
Geovanni Chavez <geovannijosue.chavez17@gmail.com>
2024-02-28 23:33:35 -0600

    feat: implement view models for Movies and Movie Details  ViewControllers

---
+++ AvengersMovies/AvengersMovies/Modules/MovieDetails/ViewModels/MovieDetailsViewModel.swift
@@ -0,0 +1,84 @@
+//
+//  MovieDetailsViewModel.swift
+//  AvengersMovies
+//
+//  Created by Geovanni Josue Chavez on 28/2/24.
+//
+
+import Foundation
+import UIKit
+
+class MovieDetailsViewModel {
+    
+    // Observables
+    var movie: Observable<Movie> = Observable(nil)
+    var videos: Observable<Video> = Observable(nil)
+    var crew: Observable<[Cast]> = Observable(nil)
+    var similarFilms: Observable<[SimilarResult]> = Observable(nil)
+    var streamingProvider: Observable<[WatchProvider]> = Observable(nil)
+    var rentProvider: Observable<[WatchProvider]> = Observable(nil)
+    var buyProvider: Observable<[WatchProvider]> = Observable(nil)
+    var videoItems: Observable<[VideoThumbnail]> = Observable(nil)
+    var isLoading: Observable<Bool> = Observable(false)
+    var errorMessage: Observable<String> = Observable(nil)
+    
+    // Properties
+    let moviesService: MoviesService?
+    let coordinator: MoviesCoordinator?
+    var thumbnailsUrls: [VideoThumbnail]?
+    
+    
+    init(coordinator: MoviesCoordinator, movie: Movie) {
+        self.movie.value = movie
+        self.coordinator = coordinator
+        moviesService = MoviesService()
+        thumbnailsUrls = [VideoThumbnail]()
+    }
+    
+    func retrieveMovieDetails() {
+        if isLoading.value ?? true { return }
+        guard let movie = self.movie.value else { return }
+
+        isLoading.value = true
+        let movieID = String(movie.id ?? 0)
+        moviesService?.getMovieDetails(movieId: movieID) { [weak self] result in
+            guard let self = self else { return }
+            self.isLoading.value = false
+            
+            switch result {
+            case .success(let response):
+                self.crew.value = response.credits.cast
+                self.similarFilms.value = response.similar.similarResults
+                self.streamingProvider.value = response.watchProviders.providerResults?.us?.flatrate
+                self.rentProvider.value = response.watchProviders.providerResults?.us?.rent
+                self.buyProvider.value = response.watchProviders.providerResults?.us?.buy
+                
+                // Get trailer videos
+                guard let videos = response.videos.videos else { return }
+                let trailers = videos.filter { $0.type == "Trailer" }
+                
+                for video in trailers {
+                    let thumbnailUrl = "\(Constants.APIs.youTubeThumbnailUrl)/\(video.key ?? "")/\(Constants.APIs.thumbnailResolution)"
+                    let newItem = VideoThumbnail(title: video.name, thumbnailUrl: thumbnailUrl, videoKey: video.key)
+                    thumbnailsUrls?.append(newItem)
+                }
+                self.videoItems.value = thumbnailsUrls
+                        
+            case .failure:
+                errorMessage.value = Constants.Common.labelError
+            }
+        }
+    }
+    
+    func playTrailerVideo(videoKey: String) {
+        guard let youtubeURL = URL(string: "\(Constants.APIs.youTubeAppLink)\(videoKey)") else { return }
+        
+        if UIApplication.shared.canOpenURL(youtubeURL) {
+            UIApplication.shared.open(youtubeURL, options: [:], completionHandler: nil)
+        } else {
+            guard let webUrl = URL(string: "\(Constants.APIs.youTubeUrl)\(videoKey)") else { return }
+            UIApplication.shared.open(webUrl, options: [:], completionHandler: nil)
+        }
+    }
+}
+
---
+++ AvengersMovies/AvengersMovies/Modules/Movies/ViewModels/MoviesViewModel.swift
@@ -0,0 +1,91 @@
+//
+//  MoviesViewModel.swift
+//  AvengersMovies
+//
+//  Created by Geovanni Josue Chavez on 28/2/24.
+//
+
+import Foundation
+
+class MoviesViewModel {
+    
+    private var nowPlayingPage = 1
+    private var popularPage = 1
+    private var topRatedPage = 1
+    private var upcomingPage = 1
+    
+    private let moviesService: MoviesService?
+    var coordinator: MoviesCoordinator?
+    var currentFilter: Categories?
+    
+    var moviesDataSource: Observable<[Movie]> = Observable(nil)
+    var isLoading: Observable<Bool> = Observable(false)
+    var dataRetrieved: Observable<Bool> = Observable(false)
+    var errorMessage: Observable<String> = Observable(nil)
+    
+    init(coordinator: MoviesCoordinator) {
+        self.coordinator = coordinator
+        self.moviesService = MoviesService()
+    }
+    
+    func retrieveMovies(from category: Categories) {
+        if isLoading.value ?? true { return }
+        isLoading.value = true
+        
+        if currentFilter != category {
+            moviesDataSource.value = nil
+        }
+        currentFilter = category
+        
+        switch category {
+        case .nowPlaying:
+            moviesService?.getNowPlayingMovies(page: nowPlayingPage) { [weak self] result in
+                guard let self = self else { return }
+                self.isLoading.value = false
+                self.nowPlayingPage += 1
+                handleService(result: result, category: category)
+            }
+        case .popular:
+            moviesService?.getPopularMovies(page: nowPlayingPage) { [weak self] result in
+                guard let self = self else { return }
+                self.isLoading.value = false
+                self.popularPage += 1
+                handleService(result: result, category: category)
+            }
+        case .topRated:
+            moviesService?.getTopRatedMovies(page: topRatedPage) { [weak self] result in
+                guard let self = self else { return }
+                self.isLoading.value = false
+                self.topRatedPage += 1
+                handleService(result: result, category: category)
+            }
+        case .upcoming:
+            moviesService?.getUpcomingMovies(page: upcomingPage) { [weak self] result in
+                guard let self = self else { return }
+                self.isLoading.value = false
+                self.upcomingPage += 1
+                handleService(result: result, category: category)
+            }
+        }
+    }
+    
+    private func handleService(result: Result<MoviesReponse, Error>, category: Categories) {
+        errorMessage.value = nil
+        /*if category != currentFilter {
+            moviesDataSource.value = nil
+        }*/
+        
+        switch result {
+        case .success(let response):
+            if moviesDataSource.value == nil {
+                moviesDataSource.value = response.movies
+            } else {
+                guard let movies = response.movies else { return }
+                moviesDataSource.value?.append(contentsOf: movies)
+            }
+        case .failure:
+            errorMessage.value = Constants.Common.labelError
+        }
+    }
+}
+
